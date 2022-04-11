3779e4c1c47771b0689e7d767325e72c21d31738
AchmadRifai <v-achmad.rifai@traveloka.com>
2022-04-11 22:16:00 +0700

    Common module router

--- routerutils/common.go
+++ routerutils/common.go
@@ -15,30 +15,32 @@ import (
 type Endpoint struct {
 	Execution func(w http.ResponseWriter, r *http.Request)
 	Method    string
+	Mdw       func(r http.Handler) http.Handler
 }
 
-func StartingService(endpoints map[string]Endpoint, mdw mux.MiddlewareFunc) {
+func StartingService(endpoints map[string]Endpoint) {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
 	r := mux.NewRouter()
-	if mdw != nil {
-		r.Use(mdw)
-	}
 	var methods []string
 	for url, endpoint := range endpoints {
 		r.HandleFunc(url, endpoint.Execution).Methods(endpoint.Method)
+		if endpoint.Mdw != nil {
+			http.Handle(url, endpoint.Mdw(r))
+		}
 		if !commonutils.ArrayExists(methods, func(v string) bool { return v == endpoint.Method }) {
 			methods = append(methods, endpoint.Method)
 		}
 	}
 	port, path, headers, origins := os.Getenv("SERVICE.PORT"), os.Getenv("CONTEXT.PATH"), os.Getenv("ALLOWED.HEADERS"), os.Getenv("ORIGINS")
-	r.Path(path)
+	r.PathPrefix(path)
 	log.Fatal(
 		http.ListenAndServe(":"+port,
 			handlers.CORS(
 				handlers.ExposedHeaders(strings.Split(headers, ",")),
 				handlers.AllowedMethods(methods),
 				handlers.AllowedOrigins(strings.Split(origins, ",")))(r)))
+	log.Println("Deployed on port " + port + " and with context path " + path)
 }

68a9afdc0f8853ee3c4270ad6b5712168ea21846
AchmadRifai <v-achmad.rifai@traveloka.com>
2022-04-12 13:49:18 +0700

    add global error

--- errorutils/errors.go
+++ errorutils/errors.go
@@ -1,1 +1,36 @@
 package errorutils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/possawang/go-service-lib-common/domain"
+)
+
+func Handle4xx(w http.ResponseWriter, msg string, code int, status string) {
+	log.Printf("Invalidate %s\n", msg)
+	response := domain.BaseResponse[any]{
+		Data:       nil,
+		StatusCode: strconv.Itoa(code),
+		Msg:        msg,
+	}
+	handleError(w, response, status)
+}
+
+func Handle5xx(w http.ResponseWriter, e error, code int, status string) {
+	log.Printf("Internal error %s\n", e)
+	response := domain.BaseResponse[any]{
+		Data:       nil,
+		StatusCode: strconv.Itoa(code),
+		Msg:        e.Error(),
+	}
+	handleError(w, response, status)
+}
+
+func handleError(w http.ResponseWriter, response domain.BaseResponse[any], status string) {
+	json, _ := json.Marshal(response)
+	w.Header().Set("status", status)
+	w.Write(json)
+}
